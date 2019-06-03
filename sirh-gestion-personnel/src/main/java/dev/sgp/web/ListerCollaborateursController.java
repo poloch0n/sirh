5a62973aee36b59ef32bcf39087aa6039706d70e
poloch0n <50014021+poloch0n@users.noreply.github.com>
2019-06-03 10:13:36 +0200

    first page jsp

--- sirh-gestion-personnel/src/main/java/dev/sgp/web/ListerCollaborateursController.java
+++ sirh-gestion-personnel/src/main/java/dev/sgp/web/ListerCollaborateursController.java
@@ -12,13 +12,6 @@ public class ListerCollaborateursController extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		String avecPhotoParam = req.getParameter("avecPhoto");
-		// recupere la valeur d'un
-		// parametre dont le nom est departementString departement
-		String departementParam = req.getParameter("departement");
-		resp.setContentType("text/html");
-		
-		// code HTML
-		resp.getWriter().write("<h1>Liste des collaborateurs</h1>"+ "<ul>"+ "<li>avecPhoto="+ avecPhotoParam + "</li>"+ "<li>departement="+ departementParam + "</li>"+ "</ul>");
+		req.getRequestDispatcher("/WEB-INF/views/collab/listerCollaborateurs.jsp").forward(req, resp);
 	}
 }
